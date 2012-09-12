2021ab01f4544242941061d277fbd1660b3608d8
Fepe <fepe55@gmail.com>
2012-09-12 17:09:58 -0300

    Arreglado un error que se daba si había lineas vacías

--- test-lexico/scanner.py
+++ test-lexico/scanner.py
@@ -5,16 +5,17 @@ import lexico
 def scanner (fuente, listado, terminal, S, cad, restante, numero_de_linea):
     if restante is "EOF":
         S = "_FIN"
-        return (listado,S,cad,restante,numero_de_linea) 
+        return (listado,S,cad,restante,numero_de_linea)
     else:
         if not restante:
             numero_de_linea+=1
             restante = fuente.leer_linea_sin_nl()
-            listado.escribir("\n")
-            listado.escribir(str(numero_de_linea)+': '+restante)
-            (S,cad,restante) = lexico.obtener_simbolo (restante,numero_de_linea)
+            if restante:
+                listado.escribir("\n")
+                listado.escribir(str(numero_de_linea)+': '+restante)
+                (S,cad,restante) = lexico.obtener_simbolo(restante,numero_de_linea)
         else:
-            (S,cad,restante) = lexico.obtener_simbolo (restante,numero_de_linea)
+            (S,cad,restante) = lexico.obtener_simbolo(restante,numero_de_linea)
 
         return (listado,S,cad,restante,numero_de_linea)
 
